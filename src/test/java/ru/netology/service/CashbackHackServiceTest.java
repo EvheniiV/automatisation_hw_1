76a1ff51f1fa842a4554f5bc640a6d6cb2fb7d14
Jenyokgm <jenyokgm@gmail.com>
2023-08-10 11:11:52 +0300

    testng tests final

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,28 +2,25 @@ package ru.netology.service;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
+
     @Test
     public void lowerLimitValueamountTest() {
-        //CashbackHackService service = new CashbackHackService();
 
         Assert.assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void upperLimitValueamountTest() {
-        //CashbackHackService service = new CashbackHackService();
 
         Assert.assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void inRangeValueamountTest() {
-        //CashbackHackService service = new CashbackHackService();
 
         Assert.assertEquals(service.remain(600), 400);
     }
